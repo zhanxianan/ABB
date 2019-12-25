2349ee6b74e865f79dec9dcf31ad66932aa1a136
huangkaixin711 <3619373610@qq.com>
2019-12-25 13:21:11 +0800

    提交
    Signed-off-by: huangkaixin711 <3619373610@qq.com>

---
+++ HKX/animate.js
@@ -0,0 +1,43 @@
+/*
+* @Author: 黄凯欣
+* @Date:   2019-12-21 22:33:56
+* @Last Modified by:   黄凯欣
+* @Last Modified time: 2019-12-22 11:04:19
+*/
+
+function getStyle(obj, attr){
+	if(obj.currentStyle){
+		return obj.currentStyle[attr];
+	} else {
+		return getComputedStyle(obj, null)[attr];
+	}
+}
+function animate(obj,json,callback){
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function(){
+		var isStop = true;
+		for(var attr in json){
+			var now = 0;
+			if(attr == 'opacity'){
+				now = parseInt(getStyle(obj,attr)*100);
+			}else{
+				now = parseInt(getStyle(obj,attr));
+			}
+			var speed = (json[attr] - now) / 8;
+			speed = speed>0?Math.ceil(speed):Math.floor(speed);
+			var cur = now + speed;
+			if(attr == 'opacity'){
+				obj.style[attr] = cur / 100;
+			}else{
+				obj.style[attr] = cur + 'px';
+			}
+			if(json[attr] !== cur){
+				isStop = false;
+			}
+		}
+		if(isStop){
+			clearInterval(obj.timer);
+			callback&&callback();
+		}
+	}, 30)
+}
